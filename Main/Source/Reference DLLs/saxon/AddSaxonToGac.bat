5b01c69d21401562cea9b53f393a9778529f93b8
VP-DEV-DNA-1\ParnwM01 <VP-DEV-DNA-1\ParnwM01>
2010-01-06 13:12:17 +0000

    Added saxon gac install batch file and changed references to use this and not local copy

---
+++ Main/Source/Reference DLLs/saxon/AddSaxonToGac.bat
@@ -0,0 +1,23 @@
+cd ../CertsAndGACUtils/
+gacutil -u ../saxon/saxon9api.dll
+gacutil -i ../saxon/saxon9api.dll 
+gacutil -l saxon9api
+
+gacutil -u ../saxon/saxon9.dll
+gacutil -i ../saxon/saxon9.dll
+gacutil -l saxon9
+
+gacutil -u ../saxon/saxon9sa.dll
+gacutil -i ../saxon/saxon9sa.dll
+gacutil -l saxon9sa
+
+gacutil -u ../saxon/IKVM.Runtime.dll
+gacutil -i ../saxon/IKVM.Runtime.dll
+
+
+gacutil -u ../saxon/charsets.dll
+gacutil -i ../saxon/charsets.dll
+
+
+
+pause
