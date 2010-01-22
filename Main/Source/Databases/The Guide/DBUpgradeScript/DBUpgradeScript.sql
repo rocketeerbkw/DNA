d99b62f71e55ac72d5fd6d9c7498585d0c1062ee
VP-DEV-DNA-1\FrancS04 <VP-DEV-DNA-1\FrancS04>
2010-01-22 13:49:25 +0000

    Added MinCharLimit to the Comments - sets site option "CommentForum", "MinCommentCharacterLength"

--- Main/Source/Databases/The Guide/DBUpgradeScript/DBUpgradeScript.sql
+++ Main/Source/Databases/The Guide/DBUpgradeScript/DBUpgradeScript.sql
@@ -3332,6 +3332,10 @@ EXEC dbu_dosql N'MarcusP: 397ED2DE-1EA2-4d99-8F54-D494E81735FB',
 	IF (@curerror <> 0) RETURN
 END
 
+EXEC dbu_dosql N'SPF: {F15C9295-241E-4EAF-A18A-26DF70BD96E0}',
+N'exec dbu_createsiteoption 0, ''CommentForum'', ''MinCommentCharacterLength'', ''0'' ,0,''Sets the minimum character length for a comment post - 0 denotes no limit''',
+N'Creating SiteOption MinCommentCharacterLength', @curerror OUTPUT
+IF (@curerror <> 0) RETURN
 
 ------------------------------------------------------------------------------------------------
 -- INSERT NEW CODE BEFORE HERE!!!
