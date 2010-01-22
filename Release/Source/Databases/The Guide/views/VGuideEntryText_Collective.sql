76ac33e8d682c46c92e6f612654c0c42b1ca80ec
VP-DEV-DNA-1\NevesM31 <VP-DEV-DNA-1\NevesM31>
2010-01-22 13:17:30 +0000

    Merged changes to remove collective view

--- Release/Source/Databases/The Guide/views/VGuideEntryText_Collective.sql
+++ Release/Source/Databases/The Guide/views/VGuideEntryText_Collective.sql
@@ -1,9 +1,9 @@
-CREATE VIEW VGuideEntryText_collective WITH SCHEMABINDING
+/*CREATE VIEW VGuideEntryText_collective WITH SCHEMABINDING
 AS
-	/* 
+	/
 		Indexed view materialising GuideEntry id, subject and text for full-text searching. 
 		The name of the view maps onto the Site's URLName so decisions about which catalogue to query can be done programatically. 
-	*/
+	/
 
 	SELECT	ge.EntryID,
 			ge.Subject,
@@ -11,12 +11,12 @@ AS
 	  FROM	dbo.Sites s
 			INNER JOIN dbo.GuideEntries ge ON s.SiteID = ge.SiteID
 	 WHERE	s.URLName = 'collective'
-
+*/
 GO
-
+/*
 CREATE UNIQUE CLUSTERED INDEX [IX_VGuideEntryText_collective] ON [dbo].[VGuideEntryText_collective] 
 (
 	[EntryID]
 )
-
+*/
 GO
