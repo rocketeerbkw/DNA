44949a47738597f3828188ae0321c6efaf8a2bf4
VP-DEV-DNA-1\NevesM31 <VP-DEV-DNA-1\NevesM31>
2010-03-11 10:07:19 +0000

    The first version of BBC.DNA.Monitoring, that's used for the generation of Zenoss KPIs.

---
+++ Main/Source/BBC.DNA.Monitoring/BBC.DNA.Monitoring/KPIList.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BBC.DNA.Monitoring
+{
+    public class KPIList
+    {
+        public KPIList(string appName, string serverName) { _appName = appName;  _serverName = serverName; }
+
+        private string _serverName;
+        private string _appName;
+        private List<KPI> _listOfKPIs = new List<KPI>();
+
+        public List<KPI> ListOfKPIs { get { return _listOfKPIs; } }
+        public string ServerName { get { return _serverName; } }
+        public string AppName { get { return _appName; } }
+    }
+}
---
+++ Main/Source/BBC.DNA.Monitoring/BBC.DNA.Monitoring/ZenossKPIThreshold.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BBC.DNA.Monitoring
+{
+    public class ZenossKPIThreshold
+    {
+        string _name;
+        int _threshold;
+
+        public string Name   { get { return _name; }      set { _name = value;  } }
+        public int Threshold { get { return _threshold; } set { _threshold = value;  } }
+    }
+}
