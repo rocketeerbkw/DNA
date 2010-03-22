1354098ebc58dbb1bde6af3ab4cfedd61185b374
VP-DEV-DNA-1\HowitM01 <VP-DEV-DNA-1\HowitM01>
2010-03-22 16:32:22 +0000

    Updated FetchGroupsAndMembers.sql so that it only returns non NULL data rows. Added staored procedure tests

--- Main/Source/Databases/The Guide/storedprocedures/fetchgroupsandmembers.sql
+++ Main/Source/Databases/The Guide/storedprocedures/fetchgroupsandmembers.sql
@@ -6,4 +6,5 @@ as
 select gm.siteid, gm.groupid, g.name, gm.userid
 from groupmembers gm WITH(NOLOCK)
 inner join groups g WITH(NOLOCK) on g.groupid = gm.groupid
+WHERE gm.UserID IS NOT NULL AND gm.groupID IS NOT NULL AND gm.SiteID IS NOT NULL
 order by gm.userid, gm.siteid
