0edd5c648d2bc921d3a885ae419cffd0359038d3
VP-DEV-DNA-1\MulleW01 <VP-DEV-DNA-1\MulleW01>
2010-06-21 14:38:21 +0000

    missing file from previous checkin

---
+++ Main/Source/Shared/BBC.Dna.Objects/CategoryChildren.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BBC.Dna.Objects
+{
+    /// <remarks/>
+    [System.CodeDom.Compiler.GeneratedCodeAttribute("System.Xml", "2.0.50727.3053")]
+    [System.SerializableAttribute()]
+    [System.ComponentModel.DesignerCategoryAttribute("code")]
+    [System.Xml.Serialization.XmlTypeAttribute(AnonymousType = true)]
+    public class CategoryChildren
+    {
+        /// <remarks/>
+        // [System.Xml.Serialization.XmlElementAttribute("SubCategories", Order = 0)]
+        [System.Xml.Serialization.XmlArrayAttribute("SUBCATEGORIES", Order = 0)] 
+        public System.Collections.Generic.List<CategorySummary> SubCategories { get; set; }
+
+        /// <remarks/>
+        [System.Xml.Serialization.XmlArrayAttribute("ARTICLES", Order = 1)]
+        public System.Collections.Generic.List<ArticleSummary> Articles { get; set; }
+
+    }
+}
