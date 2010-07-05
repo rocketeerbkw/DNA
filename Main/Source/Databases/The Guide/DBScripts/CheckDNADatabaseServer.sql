fe40a9b3823bdcc9fda6eb34298bd339ee5e4c8e
VP-DEV-DNA-1\NevesM31 <VP-DEV-DNA-1\NevesM31>
2010-07-05 13:53:46 +0000

    This is the version of CheckDNADatabaseServer that is running on live, so updating the version in TFS to reflect this.  It's not a file that is deployed when we go live, so is safe to check in without a work item, or code review

--- Main/Source/Databases/The Guide/DBScripts/CheckDNADatabaseServer.sql
+++ Main/Source/Databases/The Guide/DBScripts/CheckDNADatabaseServer.sql
@@ -6,16 +6,21 @@ AS
 DECLARE @sendTo varchar(100),@emailProfile varchar(100)
 DECLARE @subjectPrefix varchar(100)
 DECLARE @maxWorkerThreads int, @maxRunnableTasks int, @SchedulerInfoAge int
+DECLARE @maxSOSWaitStat int, @maxTokenAndPermUserStoreMem int
+
+DECLARE @stage int -- used in the exception handling code
 
 --------------------------------------
 -- Set up configuration variables
-SET @subjectPrefix = 'Check DNA Database Server: '
+SET @subjectPrefix = 'DNA Database Server: '
 
-SELECT  @sendTo='mark.neves@gmail.com;mark.neves@bbc.co.uk',
-		@emailProfile='DNA Email Server Profile',
-		@maxWorkerThreads = 300, -- Number of worker threads the server is using
-		@maxRunnableTasks = 5*8, -- Total runnable tasks across all 8 CPUs
-		@SchedulerInfoAge = 120  -- The newest data in the SchedulerInfo table must be <= @SchedulerInfoAge seconds old
+SELECT  @sendTo='mark.neves@bbc.co.uk;mark.howitt@bbc.co.uk',
+		@emailProfile='GUIDE6-2 DNA Email Profile',
+		@maxWorkerThreads = 300,    -- Number of worker threads the server is using
+		@maxRunnableTasks = 5*8,    -- Total runnable tasks across all 8 CPUs
+		@SchedulerInfoAge = 120,    -- The newest data in the SchedulerInfo table must be <= @SchedulerInfoAge seconds old
+		@maxSOSWaitStat   = 400000, -- If the last SOS_SCHEDULER_YIELD wait is above this value, it sends an email
+		@maxTokenAndPermUserStoreMem = 100 -- If this cache gets too big, we clear it out
 
 BEGIN TRY
 	--------------------------------------
@@ -28,36 +33,89 @@ BEGIN TRY
 		WHERE subject=@heartBeatSubject
 		ORDER BY sent_date DESC
 
-	IF @lastHeartBeatEmail IS NULL OR DATEDIFF(mi,@lastHeartBeatEmail,getdate()) > 12
+	IF @lastHeartBeatEmail IS NULL OR DATEDIFF(hh,@lastHeartBeatEmail,getdate()) > 12
 		EXEC msdb.dbo.sp_send_dbmail @recipients=@sendTo,@subject=@heartBeatSubject,@profile_name=@emailProfile
 
 	--------------------------------------
-	-- Check runnable tasks
-	DECLARE @runnableTaskSubject varchar(100)
-	SET @runnableTaskSubject=@subjectPrefix+'Runnable Tasks'
+	-- Check for problems
+	DECLARE @lastProblemEmail DATETIME, @problemEmailSubject varchar(100)
+	SET @problemEmailSubject=@subjectPrefix+'Problem detected'
 
-	DECLARE @numRunnableTasks int, @numWorkerThreads int, @latestCapture DATETIME;
-	DECLARE @sendEmail int, @runnableTasksEmailBody varchar(max);
-	WITH LatestSchedulerInfo AS
-	(
-		SELECT TOP 8 * FROM [GUIDE5\SQL2005].Performance.dbo.SchedulerInfo ORDER BY dt DESC
-	)
-	SELECT @numRunnableTasks=SUM(runnable_task_count),@latestCapture=max(dt),@numWorkerThreads=max(num_workers)
-		FROM LatestSchedulerInfo
+	-- Find out when the last problem email was sent
+	SELECT TOP 1 @lastProblemEmail = sent_date FROM msdb.dbo.sysmail_sentitems
+		WHERE subject=@problemEmailSubject
+		ORDER BY sent_date DESC
 
-	IF DATEDIFF(s,@latestCapture,getdate()) > @SchedulerInfoAge
+	IF @lastProblemEmail IS NULL OR DATEDIFF(mi,@lastProblemEmail,getdate()) > 10
 	BEGIN
-		SELECT @sendEmail=1, @runnableTasksEmailBody='Have not captured scheduler info for over '+CAST(@SchedulerInfoAge AS varchar)+' seconds'
-	END
+set @stage=1
+		DECLARE @numRunnableTasks int, @numWorkerThreads int, @latestCapture DATETIME;
+		DECLARE @problemEmailBody varchar(max);
+		WITH LatestSchedulerInfo AS
+		(
+			SELECT TOP 8 * FROM [GUIDE6-1].Performance.dbo.SchedulerInfo ORDER BY dt DESC
+		)
+		SELECT @numRunnableTasks=SUM(runnable_task_count),@latestCapture=max(dt),@numWorkerThreads=max(num_workers)
+			FROM LatestSchedulerInfo
+set @stage=2
 
-	IF @numRunnableTasks > @maxRunnableTasks AND @numWorkerThreads > @maxWorkerThreads
-	BEGIN
-		SELECT @sendEmail=1, @runnableTasksEmailBody='Number of runnable tasks > '+cast(@maxRunnableTasks AS varchar)
-	END
+		--------------------------------------
+		-- Check that we have a recent capture, and email if we haven't
+		IF DATEDIFF(s,@latestCapture,getdate()) > @SchedulerInfoAge
+		BEGIN
+			SELECT @problemEmailBody='Have not captured scheduler info for over '+CAST(@SchedulerInfoAge AS varchar)+' seconds'
+			EXEC msdb.dbo.sp_send_dbmail @recipients=@sendTo,@subject=@problemEmailSubject,@body=@problemEmailBody,@profile_name=@emailProfile
+		END
 
-	IF @sendEmail=1
-		EXEC msdb.dbo.sp_send_dbmail @recipients=@sendTo,@subject=@runnableTaskSubject,@body=@runnableTasksEmailBody,@profile_name=@emailProfile
-		
+		--------------------------------------
+		-- Email if the number of runnable tasks is high
+		IF @numRunnableTasks > @maxRunnableTasks AND @numWorkerThreads > @maxWorkerThreads
+		BEGIN
+			SELECT @problemEmailBody='Number of runnable tasks is '+cast(@numRunnableTasks AS varchar)+'.  This indicates the database is in trouble'
+			EXEC msdb.dbo.sp_send_dbmail @recipients=@sendTo,@subject=@problemEmailSubject,@body=@problemEmailBody,@profile_name=@emailProfile
+		END
+
+		--------------------------------------
+		-- Email if the last captured SOS_SCHEDULER_YIELD signal wait time is high
+set @stage=3
+		DECLARE @lastSOSWaitStat int
+		SELECT @lastSOSWaitStat = cc.signal_wait_time-cl.signal_wait_time 
+			FROM [GUIDE6-1].SqlHealth.dbo.capture_waitstats_last cl
+			INNER JOIN [GUIDE6-1].SqlHealth.dbo.capture_waitstats_current cc ON cl.wait_type=cc.wait_type
+			WHERE cl.wait_type='SOS_SCHEDULER_YIELD'
+set @stage=4
+
+		IF @lastSOSWaitStat > @maxSOSWaitStat
+		BEGIN
+			SELECT @problemEmailBody='Last SOS_SCHEDULER_YIELD wait is '+cast(@lastSOSWaitStat AS varchar)
+			EXEC msdb.dbo.sp_send_dbmail @recipients=@sendTo,@subject=@problemEmailSubject,@body=@problemEmailBody,@profile_name=@emailProfile
+		END
+
+/***********************************************
+Commented out on 1/3/2010, as this check is not really relevent on SQL Server 2005 SP3 64bit
+
+		--------------------------------------
+		-- If the TokenAndPermUserStore is too big, email and try and shrink the cache
+set @stage=5
+		DECLARE @TokenAndPermUserStoreMem float
+		SELECT	@TokenAndPermUserStoreMem=(single_pages_kb + multi_pages_kb)/1024.0
+		FROM	[GUIDE6-1].master.sys.dm_os_memory_clerks
+		WHERE name='TokenAndPermUserStore'
+set @stage=6
+
+		IF @TokenAndPermUserStoreMem > @maxTokenAndPermUserStoreMem
+		BEGIN
+			SELECT @problemEmailBody='TokenAndPermUserStore memory use is '+cast(@TokenAndPermUserStoreMem AS varchar)+'.  Clearing the caches...'
+			EXEC msdb.dbo.sp_send_dbmail @recipients=@sendTo,@subject=@problemEmailSubject,@body=@problemEmailBody,@profile_name=@emailProfile
+
+			EXEC [GUIDE6-1].master.dbo.sp_executesql N'DBCC FREEPROCCACHE'
+			EXEC [GUIDE6-1].master.dbo.sp_executesql N'DBCC FREESYSTEMCACHE(''ALL'') WITH MARK_IN_USE_FOR_REMOVAL'
+
+			SELECT @problemEmailBody='Finished clearing the caches'
+			EXEC msdb.dbo.sp_send_dbmail @recipients=@sendTo,@subject=@problemEmailSubject,@body=@problemEmailBody,@profile_name=@emailProfile
+		END
+*****************************/
+	END		
 
 END TRY
 BEGIN CATCH
@@ -69,9 +127,7 @@ BEGIN CATCH
 		'Error state:    '+CAST(ERROR_STATE() AS VARCHAR)+CHAR(10)+
 		'Error proc:     '+ERROR_PROCEDURE()+CHAR(10)+
 		'Error line:     '+CAST(ERROR_LINE() AS VARCHAR)+CHAR(10)+
-		'Error message:  '+ERROR_MESSAGE()+CHAR(10)
+		'Error message:  '+ERROR_MESSAGE()+CHAR(10)+'stage: '+cast(@stage as varchar)
 
 	EXEC msdb.dbo.sp_send_dbmail @recipients=@sendTo,@subject='CheckDNADatabaseServer Exception',@body=@tryCatchEmail,@profile_name=@emailProfile
 END CATCH
-
-
