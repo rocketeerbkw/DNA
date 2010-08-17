9169206ede37aad8a38730bf6c905a858001cdf0
VP-DEV-DNA-1\ParnwM01 <VP-DEV-DNA-1\ParnwM01>
2010-08-17 11:16:20 +0000

    Merged deploy fix

--- Release/Source/Ripley/deployripley.bat
+++ Release/Source/Ripley/deployripley.bat
@@ -10,7 +10,7 @@ set varServerName=%4
 echo ServerName: "%varServerName%"
 set varBuildConfig=%5
 echo BuildConfig: "%varBuildConfig%"
-set varRobocopyParams=/E /XF web.config /R:1
+set varRobocopyParams=RipleyServer.dll /E /XF web.config /R:1
 
 echo Stopping IIS
 sc \\%varServerName% stop w3svc
@@ -18,15 +18,11 @@ sc \\%varServerName% stop "memcached server"
 sleep 5
 
 echo Copying Ripley
-robocopy %varBinaryRoot%\Win32\%varBuildConfig% %varServerLocation%\ %varRobocopyParams%
+robocopy %varBinaryRoot%\Win32\%varBuildConfig%\ %varServerLocation%\ %varRobocopyParams%
 
 echo Starting IIS
 sc \\%varServerName% start "memcached server"
 sc \\%varServerName% start w3svc
-
-echo Deploying Filter.reg
-REG IMPORT %varFilterReg%
-REG COPY "HKLM\SOFTWARE\The Digital Village\h2g2 web server\URLmap\Prefixes" "\\%varServerName%\HKLM\SOFTWARE\The Digital Village\h2g2 web server\URLmap\Prefixes" /f /s
 
 echo ErrorLevel: %ERRORLEVEL%
 exit %ERRORLEVEL% 
