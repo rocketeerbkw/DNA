7abef00e2b45617c37fa007534a85a9db1148082
VP-DEV-DNA-1\HouslA01 <VP-DEV-DNA-1\HouslA01>
2010-09-24 13:02:46 +0000

    Fixed speed bump Javascript.

--- Main/Source/ApacheAssets/dnaimages/javascript/DNA.js
+++ Main/Source/ApacheAssets/dnaimages/javascript/DNA.js
@@ -4,6 +4,42 @@ gloader.load(
 			async: true,
 			onLoad: function(glow) {
 				glow.ready(function() {
+					
+					//speed bump countdown
+					var bumper;
+					
+					if (glow.dom.get('p').hasClass('countdown'))
+					{
+						bumper = glow.dom.get('p.countdown');
+					}
+	                    
+	                if (bumper != null)
+	                {
+						var speedBumpTimeLeft = glow.dom.get('span#totalSeconds').text();
+		        	   
+				        speedBumpTime = setInterval(function() 
+			            {
+			        		--speedBumpTimeLeft;
+			        		
+			        		var speedBumpMinsLeft = Math.floor(speedBumpTimeLeft/60);
+			        		var speedBumpSecsLeft = speedBumpTimeLeft % 60;
+			        		var minsText = 'minutes';
+			        		
+			        		if (speedBumpMinsLeft == 1)
+			        		{
+			        			minsText = 'minute';
+			        		}
+			                
+			                bumper.html('<strong>You must wait ' + speedBumpMinsLeft + ' ' + minsText + ' ' + speedBumpSecsLeft + ' secs before you can post again</strong>');
+			                
+			                if (speedBumpTimeLeft == 0)
+			                {
+			                    clearInterval(self.speedBumpTime);
+			                    bumper.empty();
+			                }
+			                
+			            }, 1000);
+                    }
 					glow.events.addListener(
 						'.popup',
 						'click',
@@ -30,27 +66,22 @@ gloader.load(
 							return false;
 						}
 					);
-					
+                    
+                   /* glow.events.addListener(
+						'a.speedy',
+						'click',
+
+					);*/
+                    
 					glow.events.addListener(
 						'#countdown',
 						'load',
-						function() {
-							minutesSpan = glow.dom.get('#minuteValue');
-							secondsSpan = glow.dom.get('#secondValue')
-							minutes = minutesSpan.text;
-						    seconds = secondsSpan.text;
-						    inSeconds = (minutes*60) + seconds;
-						    timeNow = inSeconds - 1;
-						    
-						    if (timeNow > 0){
-						      scratchPad = timeNow / 60;
-						      minutesNow = Math.floor(scratchPad);
-						      secondsNow = (timeNow - (minutesNow * 60));
-						      
-						      minutesSpan.text(minutesNow);
-						      secondsSpan.text(secondsNow);
-						    }
-						}
+					    function()
+					    {
+							alert('setspeedbump');
+					        
+					    }					    
+						
 					);
 					if (typeof identity !== 'undefined') {
 						glow.events.addListener(identity,'logout',function(){
