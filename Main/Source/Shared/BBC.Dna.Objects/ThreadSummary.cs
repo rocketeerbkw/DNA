c52b644a31faad5ba8d2e3ce48631ea550dbe52c
VP-DEV-DNA-1\ParnwM01 <VP-DEV-DNA-1\ParnwM01>
2010-11-12 15:39:04 +0000

    Fix for caching forum threads - improved applied user settings logic and created clone on exit of cache

--- Main/Source/Shared/BBC.Dna.Objects/ThreadSummary.cs
+++ Main/Source/Shared/BBC.Dna.Objects/ThreadSummary.cs
@@ -116,9 +116,12 @@ namespace BBC.Dna.Objects
         /// <param name="site">The current site</param>
         public void ApplyUserSettings(IUser user, ISite site)
         {
-            if (user.IsEditor || user.IsSuperUser)
+            if (user != null)
             {
-                return;
+                if (user.IsEditor || user.IsSuperUser)
+                {
+                    return;
+                }
             }
             //check site is open
             if (site.IsEmergencyClosed || site.IsSiteScheduledClosed(DateTime.Now))
