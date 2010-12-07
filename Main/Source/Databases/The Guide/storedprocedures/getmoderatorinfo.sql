41cc94c29d5b346488542d0ff8e468bd8b314596
VP-DEV-DNA-1\HowitM01 <VP-DEV-DNA-1\HowitM01>
2010-12-07 12:47:48 +0000

    Updated the ordering so that the modeclassid is before the site description. This fixes the issues with the moderate page. The C++ requires the results to be ordered by classid.

--- Main/Source/Databases/The Guide/storedprocedures/getmoderatorinfo.sql
+++ Main/Source/Databases/The Guide/storedprocedures/getmoderatorinfo.sql
@@ -13,7 +13,8 @@ select	u.UserID,
 		'IsModerator' = CASE WHEN m.UserID IS NULL THEN 0 ELSE 1 END,
 		cm.ModClassID, 
 		'SiteClassID' = s.ModClassID,
-		gm.SiteID
+		gm.SiteID,
+		s.Description
 from users u
 inner join GroupMembers gm on u.UserID = gm.UserID
 inner join Sites s ON s.SiteID = gm.SiteID
@@ -36,12 +37,13 @@ select	u.UserID,
 		'IsModerator' = CASE WHEN m.UserID IS NULL THEN 0 ELSE 1 END,
 		cm.ModClassID,
 		'SiteClassID' = s.ModClassID,
+		NULL,
 		NULL
 from users u
 inner join Moderators m on u.userid = m.userid
 left join ModerationClassMembers cm on u.UserID = cm.UserID
 left join Sites s on s.ModClassID = cm.ModClassID
 where s.SiteID IS NULL and u.userid = @userid
-order by u.UserID, cm.ModClassID, s.ModClassID, gm.SiteID
+order by u.UserID, cm.ModClassID, s.ModClassID, s.description, gm.SiteID
 
 
