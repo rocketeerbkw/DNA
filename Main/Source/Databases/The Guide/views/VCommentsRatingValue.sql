7bd1c0aad755b57f32262f9341b8969a0ba6fd93
VP-DEV-DNA-1\ParnwM01 <VP-DEV-DNA-1\ParnwM01>
2010-12-09 15:20:50 +0000

    Fix for CI build

--- Main/Source/Databases/The Guide/views/VCommentsRatingValue.sql
+++ Main/Source/Databases/The Guide/views/VCommentsRatingValue.sql
@@ -3,5 +3,14 @@ WITH SCHEMABINDING
 AS
 SELECT     entryid, forumid, sum(value) AS value, count_big(*) as numberofratings
 FROM         [dbo ].[ThreadEntryRating]
-GROUP BY entryid,forumid 
-                    
+GROUP BY entryid,forumid                
+GO
+CREATE UNIQUE CLUSTERED INDEX IX_VCommentsRatingValue ON [dbo].[VCommentsRatingValue] 
+(
+	entryid
+)
+GO
+CREATE INDEX IX_VCommentsRatingValue_Forumid ON [dbo].[VCommentsRatingValue] 
+(
+	forumid
+)
