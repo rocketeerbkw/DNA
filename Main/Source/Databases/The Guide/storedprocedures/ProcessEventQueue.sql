b96f18a19d81523a9d1802742d735ead31376159
VP-DEV-DNA-1\NevesM31 <VP-DEV-DNA-1\NevesM31>
2011-02-08 13:23:05 +0000

    Added a loop facility to ProcessEventQueue so it can run more frequently than once a minute.  Needed for RiskMod

--- Main/Source/Databases/The Guide/storedprocedures/ProcessEventQueue.sql
+++ Main/Source/Databases/The Guide/storedprocedures/ProcessEventQueue.sql
@@ -1,35 +1,49 @@
-CREATE PROCEDURE processeventqueue
+CREATE PROCEDURE processeventqueue @numiterations int=6, @sleeptimeinsecs int=10
 AS
 BEGIN
-	BEGIN TRANSACTION
 	BEGIN TRY
+		DECLARE @sleeptime DATETIME
+		SET @sleeptime=DATEADD(ss,@sleeptimeinsecs,'00:00:00')
+
+		WHILE (@numiterations > 0)
+		BEGIN
+
+			BEGIN TRANSACTION
 	
-		DECLARE @TopEventID INT
-		SELECT @TopEventID = MAX(EventID) FROM dbo.EventQueue
+			DECLARE @TopEventID INT
+			SELECT @TopEventID = MAX(EventID) FROM dbo.EventQueue
 
-		-- Update the EMailEventQueue with items from the eventqueue that matches any users alerts
-		EXEC dbo.generateemailevents @TopEventID
+			-- Update the EMailEventQueue with items from the eventqueue that matches any users alerts
+			EXEC dbo.generateemailevents @TopEventID
 		
-		-- Update the SNeSActivityQueue with appropriate EventQueue items
-		EXEC dbo.generatesnesevents @TopEventID
+			-- Update the SNeSActivityQueue with appropriate EventQueue items
+			EXEC dbo.generatesnesevents @TopEventID
 		
-		EXEC generateexmodevents @TopEventID
+			EXEC generateexmodevents @TopEventID
 
-		-- Update the BIEventQueue with the events relevent to BI
-		EXEC dbo.generatebievents @TopEventID
+			-- Update the BIEventQueue with the events relevent to BI
+			EXEC dbo.generatebievents @TopEventID
 		
-		-- Update the generatesiteevents with the events
-		EXEC dbo.generatesiteevents @TopEventID
+			-- Update the generatesiteevents with the events
+			EXEC dbo.generatesiteevents @TopEventID
 
-		-- Clear the EventQueue
-		DELETE FROM dbo.EventQueue WHERE EventID <= @TopEventID
+			-- Clear the EventQueue
+			DELETE FROM dbo.EventQueue WHERE EventID <= @TopEventID
 		
-		COMMIT TRANSACTION
+			COMMIT TRANSACTION
+
+			SET @numiterations=@numiterations-1
+
+			IF @numiterations > 0
+				WAITFOR DELAY @sleeptime
+		END
 		
 	END TRY
 	BEGIN CATCH
 
-		ROLLBACK TRANSACTION
+		if @@TRANCOUNT > 0
+			ROLLBACK TRAN
+
 		RETURN ERROR_NUMBER()
 		
 	END CATCH
