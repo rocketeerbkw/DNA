06937cdc4213d01e5f96b70287dbac7234dbdd5c
VP-DEV-DNA-1\TFSBUILD <VP-DEV-DNA-1\TFSBUILD>
2011-03-23 12:01:45 +0000

    Update Version number 3.5.58.0

--- Release/Tools/SkinChecker/SkinChecker/Properties/AssemblyInfo.cs
+++ Release/Tools/SkinChecker/SkinChecker/Properties/AssemblyInfo.cs
@@ -30,4 +30,4 @@ using System.Runtime.InteropServices;
 //      Revision
 //
 [assembly: AssemblyVersion("1.1.0.0")]
-[assembly: AssemblyFileVersion("3.5.57.3")]
+[assembly: AssemblyFileVersion("3.5.58.0")]
