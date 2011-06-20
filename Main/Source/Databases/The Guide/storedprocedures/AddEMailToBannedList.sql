c7b0673d62729360624b9a102658d0fa8bfdf70a
VP-DEV-DNA-1\ParnwM01 <VP-DEV-DNA-1\ParnwM01>
2011-06-20 10:02:50 +0000

    Fix for encryption work

--- Main/Source/Databases/The Guide/storedprocedures/AddEMailToBannedList.sql
+++ Main/Source/Databases/The Guide/storedprocedures/AddEMailToBannedList.sql
@@ -4,7 +4,8 @@ DECLARE @duplicate int
 SET @duplicate = 1
 IF NOT EXISTS (SELECT * FROM dbo.BannedEmails WHERE Email = @email)
 BEGIN
-	INSERT INTO dbo.BannedEmails SELECT 'Email' = @email, 'DateAdded' = GetDate(), 'EditorID' = @editorid, 'SignInBanned' = @signinbanned, 'ComplaintBanned' = @complaintbanned
+	INSERT INTO dbo.BannedEmails 
+	SELECT 'Email' = @email, 'DateAdded' = GetDate(), 'EditorID' = @editorid, 'SignInBanned' = @signinbanned, 'ComplaintBanned' = @complaintbanned, null, null
 	SET @duplicate = 0
 END
 SELECT 'Duplicate' = @duplicate
