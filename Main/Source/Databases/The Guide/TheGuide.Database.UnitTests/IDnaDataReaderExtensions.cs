1b8f220b963f435a2b6f8972dc7ae3852b328ef2
VP-DEV-DNA-1\NevesM31 <VP-DEV-DNA-1\NevesM31>
2011-06-21 14:39:29 +0000

    To fix build

---
+++ Main/Source/Databases/The Guide/TheGuide.Database.UnitTests/IDnaDataReaderExtensions.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using BBC.Dna.Data;
+using System.Transactions;
+
+namespace TheGuide.Database.UnitTests
+{
+    public static class IDnaDataReaderExtensions
+    {
+        /// <summary>
+        /// This will throw an exception if there is not an active ambient transaction
+        /// </summary>
+        private static void CheckActiveTransactionExists()
+        {
+            try
+            {
+                var t = Transaction.Current;
+                var ti = t.TransactionInformation;
+                var tis = ti.Status;
+
+                if (tis != TransactionStatus.Active)
+                {
+                    throw new Exception("There must be an active ambient transaction");
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("There must be an active ambient transaction", ex);
+            }
+        }
+
+        public static void ExecuteWithinATransaction(this IDnaDataReader reader, string sql)
+        {
+            CheckActiveTransactionExists();
+
+            reader.ExecuteDEBUGONLY(sql);
+        }
+    }
+}
