ef9385de0043a545e56fe30a72fa30a4ae012d71
VP-DEV-DNA-1\ParnwM01 <VP-DEV-DNA-1\ParnwM01>
2011-06-23 09:30:09 +0000

    Changes to use the new shares

--- Main/deployNArthur2.bat
+++ Main/deployNArthur2.bat
@@ -1,4 +1,4 @@
 echo Deploying to NAthur2
 pause
-call deploywebserviceslive.bat wwwroot \\192.168.239.12\d$\Inetpub\wwwroot\ filter.reg 192.168.239.12 >deploy_NAthur2.txt
+call deploywebserviceslive.bat wwwroot \\192.168.239.12\Inetpub\wwwroot\ filter.reg 192.168.239.12 >deploy_NAthur2.txt
 pause
