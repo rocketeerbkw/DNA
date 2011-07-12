8db62fddb001a06f8bc5df03cfc2ae9b58e94db1
VP-DEV-DNA-1\NevesM31 <VP-DEV-DNA-1\NevesM31>
2011-07-12 16:19:21 +0000

    The output file is now different for int, test and stable deployments

--- Main/deployExtdev.bat
+++ Main/deployExtdev.bat
@@ -3,8 +3,8 @@ pause
 call deploywebserviceslive.bat wwwroot \\192.168.239.15\d$\Inetpub\wwwroot\dna-int\ filter.reg 192.168.239.15 >deploy_extdevint.txt
 echo Deploying to Extdev test
 pause
-call deploywebserviceslive.bat wwwroot \\192.168.239.15\d$\Inetpub\wwwroot\dna-test\ filter.reg 192.168.239.15 >deploy_extdevint.txt
+call deploywebserviceslive.bat wwwroot \\192.168.239.15\d$\Inetpub\wwwroot\dna-test\ filter.reg 192.168.239.15 >deploy_extdevtest.txt
 echo Deploying to Extdev
 pause
-call deploywebserviceslive.bat wwwroot \\192.168.239.15\d$\Inetpub\wwwroot\ filter.reg 192.168.239.15 >deploy_extdevint.txt
+call deploywebserviceslive.bat wwwroot \\192.168.239.15\d$\Inetpub\wwwroot\ filter.reg 192.168.239.15 >deploy_extdevstable.txt
 pause
