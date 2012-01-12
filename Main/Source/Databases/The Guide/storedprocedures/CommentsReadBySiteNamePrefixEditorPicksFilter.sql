dfbea05e0449dfa3eddd2376f6c3eb89124a2798
VP-DEV-DNA-1\ParnwM01 <VP-DEV-DNA-1\ParnwM01>
2012-01-12 16:02:39 +0000

    Have changed the join order to use post date not index

--- Main/Source/Databases/The Guide/storedprocedures/CommentsReadBySiteNamePrefixEditorPicksFilter.sql
+++ Main/Source/Databases/The Guide/storedprocedures/CommentsReadBySiteNamePrefixEditorPicksFilter.sql
@@ -36,7 +36,7 @@ as
 	
 	;with cte_usersposts as
 	(
-		select row_number() over ( order by te.threadid, te.PostIndex asc) as n, te.EntryID
+		select row_number() over ( order by te.dateposted asc) as n, te.EntryID
 		from dbo.ThreadEntries te
 		INNER JOIN dbo.CommentForums cf ON cf.ForumID = te.ForumID
 		INNER JOIN ThreadEntryEditorPicks ed ON ed.entryid = te.entryid
@@ -46,7 +46,7 @@ as
 
 		union all
 
-		select row_number() over ( order by te.threadid, te.PostIndex desc) as n, te.EntryID
+		select row_number() over ( order by te.dateposted desc) as n, te.EntryID
 		from dbo.ThreadEntries te
 		INNER JOIN dbo.CommentForums cf ON cf.ForumID = te.ForumID
 		INNER JOIN ThreadEntryEditorPicks ed ON ed.entryid = te.entryid
