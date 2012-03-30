b9854bd85a20e0f6c3fa78c3aceb2405cd85ef9e
VP-DEV-DNA-1\TFSBUILD <VP-DEV-DNA-1\TFSBUILD>
2012-03-30 14:34:09 +0000

    Update Version number 3.5.84.0

--- Release/Source/Shared/BBC.Dna.SocialAPI/Properties/AssemblyInfo.cs
+++ Release/Source/Shared/BBC.Dna.SocialAPI/Properties/AssemblyInfo.cs
@@ -34,4 +34,4 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyKeyFile("BBC.Dna.snk")]
 [assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("3.5.83.1")]
+[assembly: AssemblyFileVersion("3.5.84.0")]
