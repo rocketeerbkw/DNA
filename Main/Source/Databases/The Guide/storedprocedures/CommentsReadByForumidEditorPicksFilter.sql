003b8055bb1f72cbade798dcd358aa69e0feb7e1
VP-DEV-DNA-1\thiags01 <VP-DEV-DNA-1\thiags01>
2012-05-23 16:48:28 +0000

    Retweets - DNA to return retweets in tweet feed for tweet module, https://jira.dev.bbc.co.uk/browse/SPSTWEET-266

--- Main/Source/Databases/The Guide/storedprocedures/CommentsReadByForumidEditorPicksFilter.sql
+++ Main/Source/Databases/The Guide/storedprocedures/CommentsReadByForumidEditorPicksFilter.sql
@@ -34,10 +34,30 @@ where te.forumid = @forumid
 )
 select cte_usersposts.n, 
 	vu.*,
+	case 
+		when ISNULL(tet.OriginalTweetId, 0) <> 0
+		then tet.TweetId
+		else 0
+	end as 'RetweetId',
+	case 
+		when ISNULL(tet.OriginalTweetId, 0) <> 0
+		then (select u.loginname from dbo.ThreadEntriesTweetInfo tt
+				inner join dbo.ThreadEntries te on tt.ThreadEntryId = te.EntryID
+				inner join dbo.Users u on te.UserID = u.UserID
+				where tt.TweetId = tet.TweetId)
+		else NULL
+	end as 'RetweetedBy',
+	case	
+		when ISNULL(tet.OriginalTweetId, 0) <> 0
+		then vu.text
+		else NULL
+	end as 'Retweet',
 	@totalresults as totalresults,
 	case when crv.value is null then 0 else crv.value end as nerovalue
 from cte_usersposts
-inner join VComments vu on vu.Id = cte_usersposts.EntryID
+inner join dbo.VComments vu on vu.Id = cte_usersposts.EntryID
+left join dbo.ThreadEntriesTweetInfo tet on vu.Id = tet.ThreadEntryId and tet.IsOriginalTweetForRetweet <> 1
 left join dbo.VCommentsRatingValue crv WITH(NOEXPAND)  on crv.entryid = cte_usersposts.EntryID
-where n > @startindex and n <= @startindex + @itemsPerPage
+where tet.IsOriginalTweetForRetweet <> 1 
+and n > @startindex and n <= @startindex + @itemsPerPage
 order by n
