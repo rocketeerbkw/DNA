02f4b100e72126269fbcf38d9cac5752184f8ae5
VP-DEV-DNA-1\thiags01 <VP-DEV-DNA-1\thiags01>
2012-07-24 14:46:52 +0000

    Scipts for setup news correspondents with the Tweet Modules

---
+++ Main/Source/Databases/The Guide/DBScripts/Athletes setup/Athelete_Details_FixNames.sql
@@ -0,0 +1,6 @@
+update [dbo].[AtheletesDetails] set Name = 'glenn o''shea' where TwitterUserID = '27855280'
+update [dbo].[AtheletesDetails] set Name = 'Joe O''Regan' where TwitterUserID = '279681266'
+update [dbo].[AtheletesDetails] set Name = 'Katie O''Donnell' where TwitterUserID = '363092629'
+update [dbo].[AtheletesDetails] set Name = 'Kieran O''Malley' where TwitterUserID = '100369346'
+update [dbo].[AtheletesDetails] set Name = 'Siobhan O''Connor' where TwitterUserID = '265916293'
+
