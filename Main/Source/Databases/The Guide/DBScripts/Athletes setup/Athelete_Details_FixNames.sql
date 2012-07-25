c772e218f3080cb48ebbcc76b18ea8fa79f133de
VP-DEV-DNA-1\HowitM01 <VP-DEV-DNA-1\HowitM01>
2012-07-25 14:21:08 +0000

    Updated the scripts for creating new twitter accounts so that it does them in a controlled batch number. Updated the user name script with correct casing for the user names.

--- Main/Source/Databases/The Guide/DBScripts/Athletes setup/Athelete_Details_FixNames.sql
+++ Main/Source/Databases/The Guide/DBScripts/Athletes setup/Athelete_Details_FixNames.sql
@@ -1,4 +1,4 @@
-update [dbo].[AtheletesDetails] set Name = 'glenn o''shea' where TwitterUserID = '27855280'
+update [dbo].[AtheletesDetails] set Name = 'Glenn O''Shea' where TwitterUserID = '27855280'
 update [dbo].[AtheletesDetails] set Name = 'Joe O''Regan' where TwitterUserID = '279681266'
 update [dbo].[AtheletesDetails] set Name = 'Katie O''Donnell' where TwitterUserID = '363092629'
 update [dbo].[AtheletesDetails] set Name = 'Kieran O''Malley' where TwitterUserID = '100369346'
