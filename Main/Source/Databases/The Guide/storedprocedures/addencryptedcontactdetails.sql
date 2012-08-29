d1a03fe7861deb10472f7c993bc9b36ce307d529
VP-DEV-DNA-1\HowitM01 <VP-DEV-DNA-1\HowitM01>
2012-08-29 14:45:52 +0000

    https://jira.dev.bbc.co.uk/browse/SPSDNA-126 : Checking in encryption work for the contact form tables We now encrypt the email and text for the contact forms via the current email encryption keys. All stored procedures that reference the tables have been updated with the encryption/decryption functions.

--- Main/Source/Databases/The Guide/storedprocedures/addencryptedcontactdetails.sql
+++ Main/Source/Databases/The Guide/storedprocedures/addencryptedcontactdetails.sql
@@ -1,6 +1,7 @@
-CREATE PROCEDURE addencryptedcontactdetails @postid INT, @text VARCHAR(MAX)
+CREATE PROCEDURE addencryptedcontactdetails @postid INT, @text VARCHAR(8000)
 AS
 IF EXISTS (SELECT * FROM dbo.ThreadEntries WHERE EntryID = @postid) AND NOT EXISTS (SELECT * FROM dbo.ThreadEntriesEncrypted WHERE EntryID = @postid)
 BEGIN
-	INSERT INTO dbo.ThreadEntriesEncrypted SELECT EntryID = @postid, [Text] = @text
+	EXEC openemailaddresskey
+	INSERT INTO dbo.ThreadEntriesEncrypted SELECT EntryID = @postid, [EncryptedText] = dbo.udf_encryptemailaddress(@text,@postid), [HashedText] = dbo.udf_hashemailaddress(@text)
 END
--- Main/Source/Databases/The Guide/storedprocedures/setcommentforumascontactform.sql
+++ Main/Source/Databases/The Guide/storedprocedures/setcommentforumascontactform.sql
@@ -3,13 +3,14 @@ AS
 IF EXISTS (SELECT * FROM dbo.CommentForums WHERE ForumID = @forumid)
 BEGIN
 	UPDATE dbo.CommentForums SET IsContactForm = 1 WHERE ForumID = @forumid
-	
+	EXEC openemailaddresskey
+
 	IF NOT EXISTS (SELECT * FROM dbo.ContactForms WHERE ForumID = @forumid)
 	BEGIN
-		INSERT INTO dbo.ContactForms SELECT ForumID = @forumid, EncryptedContactEmail = @contactemail
+		INSERT INTO dbo.ContactForms SELECT ForumID = @forumid, EncryptedContactEmail = dbo.udf_encryptemailaddress(@contactemail,@forumid), HashedEmail = dbo.udf_hashemailaddress(@contactemail)
 	END
 	ELSE
 	BEGIN
-		UPDATE dbo.ContactForms SET EncryptedContactEmail = @contactemail WHERE ForumID = @forumid
+		UPDATE dbo.ContactForms SET EncryptedContactEmail = dbo.udf_encryptemailaddress(@contactemail,@forumid) WHERE ForumID = @forumid
 	END
 END
