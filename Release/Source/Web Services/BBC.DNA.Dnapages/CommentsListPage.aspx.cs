1c8ee8bc2dce1aff876ff484b6e3d3dc978275b5
VP-DEV-DNA-1\HowitM01 <VP-DEV-DNA-1\HowitM01>
2012-10-08 09:29:10 +0000

    https://jira.dev.bbc.co.uk/browse/SPS-1 : Reverting the permission to the page to see if it fixes the current SSL problems.

--- Release/Source/Web Services/BBC.DNA.Dnapages/CommentsListPage.aspx.cs
+++ Release/Source/Web Services/BBC.DNA.Dnapages/CommentsListPage.aspx.cs
@@ -33,7 +33,7 @@ public partial class CommentsListPage : BBC.Dna.Page.DnaWebPage
     {
         get
         {
-            return DnaBasePage.UserTypes.EditorAndAbove;
+            return DnaBasePage.UserTypes.Any;
         }
     }
 
