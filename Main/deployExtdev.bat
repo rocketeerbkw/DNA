d7342f423b8c63d586e7dad6ce2fbb6164f72235
VP-DEV-DNA-1\HowitM01 <VP-DEV-DNA-1\HowitM01>
2012-11-30 15:29:40 +0000

    Updated the bat file for deploying to Extdev so that it copied the skins to the correct places for int, test and stable. New file added to do the hard work of copy

--- Main/deployExtdev.bat
+++ Main/deployExtdev.bat
@@ -1,10 +1,13 @@
 echo Deploying to Extdev int
 pause
-call deploywebserviceslive.bat wwwroot \\192.168.239.15\d$\Inetpub\wwwroot\dna-int\ filter.reg 192.168.239.15 >deploy_extdevint.txt
+call deploywebservicesExtdevOthers.bat wwwroot \\192.168.239.15\d$\Inetpub\wwwroot\dna-int\ filter.reg 192.168.239.15 >deploy_extdevint.txt
 echo Deploying to Extdev test
 pause
-call deploywebserviceslive.bat wwwroot \\192.168.239.15\d$\Inetpub\wwwroot\dna-test\ filter.reg 192.168.239.15 >deploy_extdevtest.txt
-echo Deploying to Extdev
+call deploywebservicesExtdevOthers.bat wwwroot \\192.168.239.15\d$\Inetpub\wwwroot\dna-test\ filter.reg 192.168.239.15 >deploy_extdevtest.txt
+echo Deploying to Extdev stable
 pause
-call deploywebserviceslive.bat wwwroot \\192.168.239.15\d$\Inetpub\wwwroot\ filter.reg 192.168.239.15 >deploy_extdevstable.txt
+call deploywebservicesExtdevOthers.bat wwwroot \\192.168.239.15\d$\Inetpub\wwwroot\dna-stable\ filter.reg 192.168.239.15 >deploy_extdevstable.txt
+echo Deploying to Extdev 
+pause
+call deploywebserviceslive.bat wwwroot \\192.168.239.15\d$\Inetpub\wwwroot\ filter.reg 192.168.239.15 >deploy_extdev.txt
 pause
---
+++ Main/deploywebservicesExtdevOthers.bat
@@ -0,0 +1,36 @@
+@echo off
+
+set varBinaryRoot=%1
+echo BinaryRoot: %varBinaryRoot%
+set varServerLocation=%2
+echo ServerLocation: %varServerLocation%
+set varFilterReg=%3
+echo FilterReg: %varFilterReg%
+set varServerName=%4
+echo ServerName: %varServerName%
+set varRobocopyParams=/E /XF web.config /R:1
+
+echo Stopping IIS
+sc \\%varServerName% stop w3svc
+sc \\%varServerName% stop "memcached server"
+sleep 5
+
+echo Copying DNA.Pages
+robocopy %varBinaryRoot%\Dnapages\ %varServerLocation%\dnapages %varRobocopyParams%
+
+echo Copying Apis
+robocopy %varBinaryRoot%\api\ %varServerLocation%\api\ %varRobocopyParams%
+
+echo Copying skins
+robocopy %varBinaryRoot%\h2g2\skins %varServerLocation%\skins %varRobocopyParams%
+
+echo Ripley
+xcopy %varBinaryRoot%\h2g2\RipleyServer.dll  %varServerLocation%\ /y
+
+echo Starting IIS
+sc \\%varServerName% start "memcached server"
+sc \\%varServerName% start w3svc
+
+echo Deploying Filter.reg
+REG IMPORT %varFilterReg%
+REG COPY "HKLM\SOFTWARE\The Digital Village\h2g2 web server\URLmap\Prefixes" "\\%varServerName%\HKLM\SOFTWARE\The Digital Village\h2g2 web server\URLmap\Prefixes" /f /s
