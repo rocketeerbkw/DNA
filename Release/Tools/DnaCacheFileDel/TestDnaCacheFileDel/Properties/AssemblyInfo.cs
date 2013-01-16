6f241ed6a31f9f34f138efc920e997e1297035d1
VP-DEV-DNA-1\TFSBUILD <VP-DEV-DNA-1\TFSBUILD>
2013-01-16 09:32:04 +0000

    Update Version number 3.5.96.0

--- Release/Tools/DnaCacheFileDel/TestDnaCacheFileDel/Properties/AssemblyInfo.cs
+++ Release/Tools/DnaCacheFileDel/TestDnaCacheFileDel/Properties/AssemblyInfo.cs
@@ -30,4 +30,4 @@ using System.Runtime.InteropServices;
 //      Revision
 //
 [assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("3.5.95.8")]
+[assembly: AssemblyFileVersion("3.5.96.0")]
