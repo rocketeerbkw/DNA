2d4e10b407e411c2f7b5a0ab9ce25ad6db053e2c
VP-DEV-DNA-1\HowitM01 <VP-DEV-DNA-1\HowitM01>
2013-01-30 15:22:09 +0000

    Checking in helper bat files for the release process

--- Release/deploy_MHC_SkinsOnly.bat
+++ Release/deploy_MHC_SkinsOnly.bat
@@ -1,48 +1,41 @@
-echo Deploying to NArthur1
+echo Are you sure you want to deploy the skins to all MHC servers?
 pause
+
+echo Deploying to NArthur1
 call deployskinslive.bat wwwroot \\192.168.239.11\Inetpub\wwwroot\ filter.reg 192.168.239.11 >deploy_NAthur1.txt
 
 echo Deploying to NArthur2
-pause
 call deployskinslive.bat wwwroot \\192.168.239.12\Inetpub\wwwroot\ filter.reg 192.168.239.12 >deploy_NAthur2.txt
 
 echo Deploying to NArthur3
-pause
 call deployskinslive.bat wwwroot \\192.168.239.13\Inetpub\wwwroot\ filter.reg 192.168.239.13 >deploy_NAthur3.txt
 
 echo Deploying to NArthur4
-pause
 call deployskinslive.bat wwwroot \\192.168.239.14\Inetpub\wwwroot\ filter.reg 192.168.239.14 >deploy_NAthur4.txt
 
 echo Deploying to NArthur8
-pause
 call deployskinslive.bat wwwroot \\192.168.239.18\Inetpub\wwwroot\ filter.reg 192.168.239.18 >deploy_NAthur8.txt
 
 echo Deploying to NArthur9
-pause
 call deployskinslive.bat wwwroot \\192.168.239.19\Inetpub\wwwroot\ filter.reg 192.168.239.19 >deploy_NAthur9.txt
 
 echo Deploying to NArthur10
-pause
 call deployskinslive.bat wwwroot \\192.168.239.20\Inetpub\wwwroot\ filter.reg 192.168.239.20 >deploy_NAthur10.txt
 
 echo Deploying to NArthur11
-pause
 call deployskinslive.bat wwwroot \\192.168.239.21\Inetpub\wwwroot\ filter.reg 192.168.239.21 >deploy_NAthur11.txt
 
 echo Deploying to NArthur12
-pause
 call deployskinslive.bat wwwroot \\192.168.239.22\Inetpub\wwwroot\ filter.reg 192.168.239.22 >deploy_NAthur12.txt
 
 echo Deploying to NArthur13
-pause
 call deployskinslive.bat wwwroot \\192.168.239.23\Inetpub\wwwroot\ filter.reg 192.168.239.23 >deploy_NAthur13.txt
 
 echo Deploying to NArthur14
-pause
 call deployskinslive.bat wwwroot \\192.168.239.24\Inetpub\wwwroot\ filter.reg 192.168.239.24 >deploy_NAthur14.txt
 
 echo Deploying to NArthur15
-pause
 call deployskinslive.bat wwwroot \\192.168.239.25\Inetpub\wwwroot\ filter.reg 192.168.239.25 >deploy_NAthur15.txt
+
+Done!
 pause
