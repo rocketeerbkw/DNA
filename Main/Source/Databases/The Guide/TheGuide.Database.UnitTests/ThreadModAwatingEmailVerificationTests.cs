8ae1b3fc5ab8d8596dafc0c33ccfcdd5b8f670f2
VP-DEV-DNA-1\thiags01 <VP-DEV-DNA-1\thiags01>
2013-03-08 17:06:02 +0000

    Fix for storing unicode text in the complainttext field of the table ThreadModAwaitingEmailVerification.

---
+++ Main/Source/Databases/The Guide/TheGuide.Database.UnitTests/ThreadModAwatingEmailVerificationTests.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Transactions;
+using TestUtils;
+using BBC.Dna.Data;
+using System.Configuration;
+
+namespace TheGuide.Database.UnitTests
+{
+    /// <summary>
+    /// Summary description for ThreadModAwatingEmailVerificationTests
+    /// </summary>
+    [TestClass]
+    public class ThreadModAwatingEmailVerificationTests
+    {
+        public ThreadModAwatingEmailVerificationTests()
+        {
+            _connectionDetails = ConfigurationManager.ConnectionStrings["Database"].ConnectionString;
+        }
+
+       private string _connectionDetails;
+
+       [TestMethod]
+       public void GetComplaintText_InputwithSpecialCharsWithoutN_ReturnsError()
+       {
+           using (new TransactionScope())
+           {
+               var complaintText = "لشروط المشاركة لأنها تحتوي على تشهير  Hi";
+
+               using (IDnaDataReader reader = StoredProcedureReader.Create("", _connectionDetails))
+               {
+                   reader.ExecuteWithinATransaction(string.Format(@"INSERT INTO ThreadModAwaitingEmailVerification ( ID, ForumID, ThreadID, PostID, DateQueued, ComplaintText, SiteID, notes) " +
+                   "VALUES ( 'BA76D71B-2F06-4173-91FE-0013B050027B', 10, 100, 1000, getdate(), '{0}', 1, 'Checking for unicode characters')", complaintText));
+
+                   reader.ExecuteWithinATransaction("select * from ThreadModAwaitingEmailVerification");
+
+                   Assert.IsTrue(reader.HasRows);
+                   Assert.IsTrue(reader.Read());
+
+                   Assert.AreNotEqual(reader.GetStringNullAsEmpty("ComplaintText"), complaintText);
+
+                   reader.Close();
+               }
+
+           }
+       }
+
+       [TestMethod]
+       public void GetComplaintText_InputwithSpecialCharsWithN_ReturnsCorrectResult()
+       {
+           using (new TransactionScope())
+           {
+               var complaintText = "لشروط المشاركة لأنها تحتوي على تشهير  Hi";
+
+               using (IDnaDataReader reader = StoredProcedureReader.Create("", _connectionDetails))
+               {
+                   reader.ExecuteWithinATransaction(string.Format(@"INSERT INTO ThreadModAwaitingEmailVerification ( ID, ForumID, ThreadID, PostID, DateQueued, ComplaintText, SiteID, notes) " +
+                   "VALUES ( 'BA76D71B-2F06-4173-91FE-0013B050027B', 10, 100, 1000, getdate(), N'{0}', 1, 'Checking for unicode characters')", complaintText));
+
+                   reader.ExecuteWithinATransaction("select * from ThreadModAwaitingEmailVerification");
+
+                   Assert.IsTrue(reader.HasRows);
+                   Assert.IsTrue(reader.Read());
+
+                   Assert.AreEqual(reader.GetStringNullAsEmpty("ComplaintText"), complaintText);
+
+                   reader.Close();
+               }
+
+           }
+       }
+
+       [TestMethod]
+       public void GetComplaintText_InputwithSP_ReturnsCorrectResult()
+       {
+           using (new TransactionScope())
+           {
+               var complaintText = "لشروط المشاركة لأنها تحتوي على تشهير  Hello";
+               var hash = "BA76D71B-2F06-4173-91FE-0013B050027B";
+
+               using (IDnaDataReader reader = StoredProcedureReader.Create("", _connectionDetails))
+               {
+                   reader.ExecuteWithinATransaction(string.Format(@"exec registerpostingcomplaint @complainantid={0},@correspondenceemail={1},@postid={2},@complainttext={3},@hash={4}", 1, "'abc@abc.abc'", 100, "N'" + complaintText + "'", "'" + hash + "'"));
+                   reader.Read();
+
+                   Assert.IsTrue(reader.DoesFieldExist("ModId"));
+                   Assert.AreNotEqual(reader.GetInt32NullAsZero("ModId"), 0);
+
+                   reader.Close();
+               }
+
+           }
+       }
+    }
+}
