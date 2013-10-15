f0c6a7a8cc7534f18d3f57448bcd83c0722c34fb
VP-DEV-DNA-1\NevesM31 <VP-DEV-DNA-1\NevesM31>
2013-10-15 13:27:46 +0000

    Added code to recreate the Dev email address key needed for SmallGuide, after the restore "Hardwired" the location of the Smallguide snapshot file in this script

--- Main/RestoreSmallguideFromTeamCityBackup.sql
+++ Main/RestoreSmallguideFromTeamCityBackup.sql
@@ -1,13 +1,7 @@
 USE MASTER
 
---Look up existing snapshot
-DECLARE @filename VARCHAR(128), @SQL nvarchar(1000)
-SELECT @filename = physical_name
-FROM sys.master_files
-WHERE database_id = DB_ID('smallguidess')
-
 -- Drop snapshot if it exists
-IF ( @filename IS NOT NULL )
+IF EXISTS(SELECT * FROM sys.master_files WHERE database_id = DB_ID('smallguidess'))
 	DROP DATABASE SmallGuideSS
 
 -- Kill all existing connections
@@ -27,11 +21,40 @@ MOVE N'sysft_VGuideEntryText_collectiveCat' TO N'D:\MSSQL\FTData\SmallGuide\VGui
 MOVE N'sysft_VGuideEntryText_memoryshareCat' TO N'D:\MSSQL\FTData\SmallGuide\VGuideEntryText_memoryshareCat0000',  
 NOUNLOAD,  REPLACE,  STATS = 10
 
--- Recreate snapshot if it existed before the restore, using the same file name
-IF ( @filename IS NOT NULL )
-BEGIN
-	PRINT 'Recreating SmallGuide SnapShot'
-	SET @SQL = 'CREATE DATABASE SmallGuideSS ON 
-	( NAME = SmallGuide, FILENAME = ''' + @filename + ''') AS SNAPSHOT OF SmallGuide'
-	EXEC sp_executeSQL @SQL
-END
+-- Recreate the Dev email address key for Smallguide
+USE SmallGuide
+DROP SYMMETRIC KEY key_EmailAddress
+DROP CERTIFICATE cert_keyProtection
+DROP MASTER KEY
+DROP TABLE Salt
+
+CREATE MASTER KEY ENCRYPTION BY PASSWORD = 'Dev Master Key PW';
+
+CREATE CERTIFICATE cert_keyProtection WITH SUBJECT = 'Key Protection';
+
+CREATE SYMMETRIC KEY key_EmailAddress WITH
+	KEY_SOURCE = 'Eh up chuck',
+	ALGORITHM = TRIPLE_DES, 
+	IDENTITY_VALUE = 'Nice day'
+	ENCRYPTION BY CERTIFICATE cert_keyProtection;
+
+-- Grant ripleyrole permissions to use the objects
+GRANT CONTROL ON CERTIFICATE::cert_keyProtection TO ripleyrole;
+GRANT VIEW DEFINITION ON SYMMETRIC KEY::key_EmailAddress TO ripleyrole;
+
+CREATE TABLE Salt
+(
+	SaltId varchar(50) CONSTRAINT PK_Salt PRIMARY KEY CLUSTERED,
+	EncryptedSalt varbinary(8000)
+)
+
+-- Add an encrypted salt value for email hashes
+OPEN SYMMETRIC KEY key_EmailAddress DECRYPTION BY CERTIFICATE cert_keyProtection;
+DECLARE @enc varbinary(8000)
+SET @enc=EncryptByKey(KEY_GUID('key_EmailAddress'),'Dev Salt')
+INSERT Salt VALUES('Email',@enc)
+
+-- Recreate snapshot 
+PRINT 'Recreating SmallGuide SnapShot'
+CREATE DATABASE SmallGuideSS ON 
+( NAME = SmallGuide, FILENAME = 'E:\MSSQL\data\smallGuideSS.ss') AS SNAPSHOT OF SmallGuide
