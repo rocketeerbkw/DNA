2bc2ea2fe7777aca5deac54c310f197ab860e1ec
VP-DEV-DNA-1\NevesM31 <VP-DEV-DNA-1\NevesM31>
2013-11-05 14:07:28 +0000

    Applies the DataMigration dacpac as well

--- Main/Source/Databases/DeployDNADb.bat
+++ Main/Source/Databases/DeployDNADb.bat
@@ -1,6 +1,7 @@
 set PublishXml=%1
 
 set SqlPackage=%ProgramFiles(x86)%\Microsoft SQL Server\110\DAC\bin\SqlPackage.exe
-"%SqlPackage%" /Action:Publish /SourceFile:DNADb.dacpac /Profile:%PublishXml% /p:DropObjectsNotInSource=False
+"%SqlPackage%" /Action:Publish /SourceFile:DNADb.dacpac               /Profile:%PublishXml% /p:DropObjectsNotInSource=False
+"%SqlPackage%" /Action:Publish /SourceFile:DNADb_DataMigration.dacpac /Profile:%PublishXml% /p:DropObjectsNotInSource=False
 
 exit /B
