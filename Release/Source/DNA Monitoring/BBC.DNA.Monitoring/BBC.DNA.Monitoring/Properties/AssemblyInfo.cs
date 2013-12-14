802ac39bdebebeffe2d4db09a9eee785091f1eef
VP-DEV-DNA-1\TFSBUILD <VP-DEV-DNA-1\TFSBUILD>
2013-12-14 20:34:59 +0000

    Update Version number 3.5.130.0

--- Release/Source/DNA Monitoring/BBC.DNA.Monitoring/BBC.DNA.Monitoring/Properties/AssemblyInfo.cs
+++ Release/Source/DNA Monitoring/BBC.DNA.Monitoring/BBC.DNA.Monitoring/Properties/AssemblyInfo.cs
@@ -33,4 +33,4 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyFileVersion("3.5.129.0")]
+[assembly: AssemblyFileVersion("3.5.130.0")]
