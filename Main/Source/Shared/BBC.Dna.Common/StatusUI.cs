dad03f901e4ab482d12049c835ed1a8676d33982
VP-DEV-DNA-1\HowitM01 <VP-DEV-DNA-1\HowitM01>
2014-06-25 19:44:00 +0000

    Updated all .net status pages to use unified/common aspx/cs files

---
+++ Main/Source/Shared/BBC.Dna.Common/StatusUI.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Xml;
+using BBC.Dna.Utils;
+using Microsoft.Practices.EnterpriseLibrary.Caching;
+using System.Text;
+using System.Reflection;
+using System.IO;
+
+namespace BBC.Dna.Common
+{
+    public class StatusUI
+    {
+        public void OutputXML(int interval, Page page, IDnaDiagnostics diagnostics)
+        {
+            XmlDocument xDoc = new XmlDocument();
+            XmlNode xmlEl = xDoc.AppendChild(xDoc.CreateElement("H2G2"));
+            xmlEl.Attributes.Append(xDoc.CreateAttribute("TYPE"));
+            xmlEl.Attributes["TYPE"].InnerText = "STATUSPAGE";
+            xmlEl = xmlEl.AppendChild(xDoc.CreateElement("STATUS-REPORT"));
+            xmlEl.AppendChild(xDoc.ImportNode(Statistics.CreateStatisticsDocument(interval).FirstChild, true));
+
+            XmlDocument xmlSignal = new XmlDocument();
+            xmlSignal.LoadXml(StringUtils.SerializeToXmlUsingXmlSerialiser(SignalHelper.GetStatus(diagnostics)));
+            xmlEl.AppendChild(xDoc.ImportNode(xmlSignal.DocumentElement, true));
+
+            try
+            {
+                var memcachedCacheManager = (MemcachedCacheManager)CacheFactory.GetCacheManager("Memcached");
+                xmlEl.AppendChild(xDoc.ImportNode(memcachedCacheManager.GetStatsXml(), true));
+            }
+            catch (Exception e)
+            {
+                var childNode = xmlEl.AppendChild(xDoc.CreateElement("MEMCACHED_STATUS"));
+                childNode.InnerText = "Error getting memcached stats:" + e.Message;
+            }
+
+            page.Response.ContentType = "text/xml";
+            page.Response.Clear();
+            page.Response.Write(xDoc.InnerXml);
+            page.Response.End();
+        }
+
+        public void OutputHTML(int interval, ref Label lblHostName, string hostName, ref Table tblStats)
+        {
+            lblHostName.Text = hostName;
+
+            XmlDocument xStats = Statistics.CreateStatisticsDocument(interval);
+            XmlNodeList nodeList = xStats.SelectNodes("/STATISTICS/STATISTICSDATA");
+            bool headerAdded = false;
+
+            //for (int i = 0; i < nodeList.Count; i++)
+            foreach (XmlNode node in nodeList)
+            {
+                TableRow row = null;
+                TableCell cell = null;
+                if (!headerAdded)
+                {
+                    CreateHeaderRow(node, ref tblStats, ref row, ref cell);
+                    headerAdded = true;
+                }
+
+                row = new TableRow();
+                cell = new TableCell();
+                cell.Style.Add("font-weight", "bold");
+                cell.Text = node.Attributes["INTERVALSTARTTIME"].InnerText;
+                row.Cells.Add(cell);
+
+                foreach (XmlElement el in node.ChildNodes)
+                {
+                    if (!el.Name.StartsWith("RSS") && !el.Name.StartsWith("HTML") && !el.Name.StartsWith("SSI"))
+                    {
+                        cell = new TableCell();
+                        cell.Text = el.InnerText;
+                        cell.HorizontalAlign = HorizontalAlign.Center;
+                        row.Cells.Add(cell);
+                    }
+                }
+                tblStats.Rows.Add(row);
+            }
+
+        }
+
+        public void AddFileinfo(ref Label lbFileInfo)
+        {
+            StringBuilder info = new StringBuilder();
+            foreach (Attribute attr in Assembly.GetExecutingAssembly().GetCustomAttributes(false))
+            {
+                if (attr.GetType() == typeof(AssemblyFileVersionAttribute))
+                {
+                    info.AppendLine("File Version : " + ((AssemblyFileVersionAttribute)attr).Version + "<br/>");
+                }
+            }
+
+            string loc = Assembly.GetExecutingAssembly().Location;
+            DateTime creationTime = File.GetCreationTime(loc);
+            DateTime lastAccessTime = File.GetLastAccessTime(loc);
+            DateTime lastWriteTime = File.GetLastWriteTime(loc);
+
+            info.AppendLine("Created : " + creationTime.ToString() + "<br/>");
+            info.AppendLine("Last Accessed : " + lastAccessTime.ToString() + "<br/>");
+            info.AppendLine("Last Written : " + lastWriteTime.ToString() + "<br/>");
+            lbFileInfo.Text = info.ToString();
+        }
+
+        private void CreateHeaderRow(XmlNode node, ref Table tblStats, ref TableRow row, ref TableCell cell)
+        {
+            //add header row
+            row = new TableRow();
+            cell = new TableCell();
+            cell.HorizontalAlign = HorizontalAlign.Center;
+            cell.Style.Add("font-weight", "bold");
+            cell.Text = "TIME";
+            row.Cells.Add(cell);
+
+            foreach (XmlElement el in node.ChildNodes)
+            {
+                if (!el.Name.StartsWith("RSS") && !el.Name.StartsWith("HTML") && !el.Name.StartsWith("SSI"))
+                {
+                    cell = new TableCell();
+                    cell.Text = el.Name;
+                    cell.HorizontalAlign = HorizontalAlign.Center;
+                    cell.Style.Add("font-weight", "bold");
+                    cell.Text = cell.Text.Replace("CACHE", "CACHE ").Replace("SERVER", "SERVER ").Replace("REQUESTS", " REQUESTS").Replace("REQUEST", " REQUEST");
+                    row.Cells.Add(cell);
+                }
+            }
+            tblStats.Rows.Add(row);
+        }
+    }
+}
--- Main/Source/Web Services/BBC.DNA.Dnapages/status.aspx.cs
+++ Main/Source/Web Services/BBC.DNA.Dnapages/status.aspx.cs
@@ -10,15 +10,49 @@ using System.Web.UI.WebControls.WebParts;
 using System.Web.UI.HtmlControls;
 using BBC.Dna;
 using BBC.Dna.Page;
+using BBC.Dna.Utils;
+using BBC.Dna.Common;
+using System.Reflection;
+using System.IO;
+using System.Text;
 
 public partial class status : DnaWebPage
 {
-	/// <summary>
+    public status()
+    {
+        UseDotNetRendering = true;
+    }
+
+    protected Label lblHostName;
+    protected Table tblStats;
+    
+    /// <summary>
 	/// General page handling
 	/// </summary>
 	public override void OnPageLoad()
 	{
-        AddComponent(new Status(_basePage));
+        int interval = 60;
+        if (!Int32.TryParse(Request.QueryString["interval"], out interval))
+        {
+            interval = 60;
+        }
+
+        if (Request.QueryString["reset"] == "1")
+        {
+            Statistics.ResetCounters();
+        }
+
+        StatusUI statusUI = new StatusUI();
+        statusUI.AddFileinfo(ref lbFileInfo);
+
+        if (Request.QueryString["skin"] == "purexml")
+        {
+            statusUI.OutputXML(interval, this, _basePage.Diagnostics);
+        }
+        else
+        {
+            statusUI.OutputHTML(interval, ref lblHostName, _basePage.CurrentServerName, ref tblStats);
+        }
 	}
 
 	/// <summary>
@@ -28,5 +62,4 @@ public partial class status : DnaWebPage
 	{
 		get { return "STATUSPAGE"; }
 	}
-
 }
