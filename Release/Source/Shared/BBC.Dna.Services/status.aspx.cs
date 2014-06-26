bebdd17eec5f629a301433b55ca87bcca7225be9
VP-DEV-DNA-1\HowitM01 <VP-DEV-DNA-1\HowitM01>
2014-06-26 07:19:20 +0000

    Merging in latest changes for Release 3.5.137.1

--- Release/Source/Shared/BBC.Dna.Services/status.aspx.cs
+++ Release/Source/Shared/BBC.Dna.Services/status.aspx.cs
@@ -12,12 +12,11 @@ using BBC.Dna.Common;
 
 namespace BBC.Dna.Services
 {
-    public class status : System.Web.UI.Page
+    public class status : Page
     {
         protected Label lblHostName;
-        protected Label lblmemcachedStats;
+        protected Label lbFileInfo;
         protected Table tblStats;
-
 
         protected void Page_Load(object sender, EventArgs e)
         {
@@ -27,100 +26,22 @@ namespace BBC.Dna.Services
                 interval = 60;
             }
 
-
-
-
             if (Request.QueryString["reset"] == "1")
             {
                 Statistics.ResetCounters();
             }
+
+            StatusUI statusUI = new StatusUI();
+            statusUI.AddFileinfo(ref lbFileInfo);
+
             if (Request.QueryString["skin"] == "purexml")
             {
-                OutputXML(interval);
+                statusUI.OutputXML(interval, this, Global.dnaDiagnostics);
             }
             else
             {
-                OutputHTML(interval);
+                statusUI.OutputHTML(interval, ref lblHostName, Request.ServerVariables["SERVER_NAME"], ref tblStats);
             }
-        }
-
-        private void OutputXML(int interval)
-        {
-            XmlDocument xDoc = new XmlDocument();
-            XmlNode xmlEl = xDoc.AppendChild(xDoc.CreateElement("H2G2"));
-            xmlEl.Attributes.Append(xDoc.CreateAttribute("TYPE"));
-            xmlEl.Attributes["TYPE"].InnerText = "STATUSPAGE";
-            xmlEl = xmlEl.AppendChild(xDoc.CreateElement("STATUS-REPORT"));
-            xmlEl.AppendChild(xDoc.ImportNode(Statistics.CreateStatisticsDocument(interval).FirstChild, true));
-
-            XmlDocument xmlSignal = new XmlDocument();
-            xmlSignal.LoadXml(StringUtils.SerializeToXmlUsingXmlSerialiser(SignalHelper.GetStatus(Global.dnaDiagnostics)));
-            xmlEl.AppendChild(xDoc.ImportNode(xmlSignal.DocumentElement, true));
-
-            try
-            {
-                var memcachedCacheManager = (MemcachedCacheManager)CacheFactory.GetCacheManager("Memcached");
-                xmlEl.AppendChild(xDoc.ImportNode(memcachedCacheManager.GetStatsXml(), true));
-            }
-            catch (Exception e)
-            {
-                var childNode = xmlEl.AppendChild(xDoc.CreateElement("MEMCACHED_STATUS"));
-                childNode.InnerText = "Error getting memcached stats:" + e.Message;
-            }
-
-            Response.ContentType = "text/xml";
-            Response.Clear();
-            Response.Write(xDoc.InnerXml);
-            Response.End();
-        }
-
-        private void OutputHTML(int interval)
-        {
-            lblHostName.Text = Request.ServerVariables["SERVER_NAME"];
-
-            XmlDocument xStats = Statistics.CreateStatisticsDocument(interval);
-            XmlNodeList nodeList = xStats.SelectNodes("/STATISTICS/STATISTICSDATA");
-            for (int i = 0; i < nodeList.Count; i++)
-            {
-                TableRow row = null;
-                TableCell cell = null;
-                if (i == 0)
-                {//add header row
-                    row = new TableRow();
-                    cell = new TableCell();
-                    cell.HorizontalAlign = HorizontalAlign.Center;
-                    cell.Style.Add("font-weight", "bold");
-                    cell.Text = "TIME";
-                    row.Cells.Add(cell);
-
-                    foreach (XmlElement el in nodeList[i].ChildNodes)
-                    {
-                        cell = new TableCell();
-                        cell.Text = el.Name;
-                        cell.HorizontalAlign = HorizontalAlign.Center;
-                        cell.Style.Add("font-weight", "bold");
-                        cell.Text = cell.Text.Replace("CACHE", "CACHE ").Replace("SERVER", "SERVER ").Replace("REQUESTS", " REQUESTS").Replace("REQUEST", " REQUEST");
-                        row.Cells.Add(cell);
-                    }
-                    tblStats.Rows.Add(row);
-                }
-
-                row = new TableRow();
-                cell = new TableCell();
-                cell.Style.Add("font-weight", "bold");
-                cell.Text = nodeList[i].Attributes["INTERVALSTARTTIME"].InnerText;
-                row.Cells.Add(cell);
-
-                foreach (XmlElement el in nodeList[i].ChildNodes)
-                {
-                    cell = new TableCell();
-                    cell.Text = el.InnerText;
-                    cell.HorizontalAlign = HorizontalAlign.Center;
-                    row.Cells.Add(cell);
-                }
-                tblStats.Rows.Add(row);
-            }
-
         }
     }
 }
