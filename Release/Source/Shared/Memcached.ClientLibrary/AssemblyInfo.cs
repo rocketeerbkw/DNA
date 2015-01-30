2f7aea61b4f9689714f2f243fa3041a3f42cadfe
VP-DEV-DNA-1\TFSBUILD <VP-DEV-DNA-1\TFSBUILD>
2015-01-30 15:42:53 +0000

    Update Version number 3.5.139.2

--- Release/Source/Shared/Memcached.ClientLibrary/AssemblyInfo.cs
+++ Release/Source/Shared/Memcached.ClientLibrary/AssemblyInfo.cs
@@ -29,7 +29,7 @@ using System;
 // by using the '*' as shown below:
 
 [assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("3.5.139.0")]
+[assembly: AssemblyFileVersion("3.5.139.2")]
 
 //
 // In order to sign your assembly you must specify a key to use. Refer to the 
