812c3893b1eb582aeb647da05b34e4b13fdc9023
aby-varghese <aby.varghese@bbc.co.uk>
2015-07-08 09:13:32 +0000

    MYMODCOMM-1078 - Fixes to get the database unit tests to pass in VS 2013

--- Main/Source/Databases/The Guide/TheGuide.Database.UnitTests/GetModerationExLinks.cs
+++ Main/Source/Databases/The Guide/TheGuide.Database.UnitTests/GetModerationExLinks.cs
@@ -21,10 +21,10 @@ namespace TheGuide.Database.UnitTests
             _connectionDetails = ConfigurationManager.ConnectionStrings["Database"].ConnectionString;
         }
 
-       private string _connectionDetails;
+        private string _connectionDetails;
 
-       [TestMethod]
-       public void GetModerationExLinks_NoItemsLocked_LocksItemsUnderUser()
+        [TestMethod]
+        public void GetModerationExLinks_NoItemsLocked_LocksItemsUnderUser()
         {
             using (new TransactionScope())
             {
@@ -64,140 +64,139 @@ namespace TheGuide.Database.UnitTests
                     }
                 }
 
-                
+
             }
         }
 
-       [TestMethod]
-       public void GetModerationExLinks_ItemsAlreadyLocked_NoLockedItemsUnderUser()
-       {
-           using (new TransactionScope())
-           {
-               var userId = TestUserAccounts.GetModeratorAccount.UserID;
-               var siteId = 1;
-               int modId = 0;
+        [TestMethod]
+        public void GetModerationExLinks_ItemsAlreadyLocked_NoLockedItemsUnderUser()
+        {
+            using (new TransactionScope())
+            {
+                var userId = TestUserAccounts.GetModeratorAccount.UserID;
+                var siteId = 1;
+                int modId = 0;
 
-               using (IDnaDataReader reader = StoredProcedureReader.Create("", _connectionDetails))
-               {
-                   reader.ExecuteDEBUGONLY("select * from sites where siteid=" + siteId);
-                   Assert.IsTrue(reader.HasRows);
-                   Assert.IsTrue(reader.Read());
+                using (IDnaDataReader reader = StoredProcedureReader.Create("", _connectionDetails))
+                {
+                    reader.ExecuteDEBUGONLY("select * from sites where siteid=" + siteId);
+                    Assert.IsTrue(reader.HasRows);
+                    Assert.IsTrue(reader.Read());
 
+                    modId = reader.GetInt32NullAsZero("ModClassID");
+                }
 
-                   modId = reader.GetInt32NullAsZero("ModClassID");
-               }
-               AddExUrl(siteId);
-               
-               using (IDnaDataReader reader = StoredProcedureReader.Create("", _connectionDetails))
-               {
-                   reader.ExecuteDEBUGONLY(string.Format("exec getmoderationexlinks @modclassid={0}, @referrals=0, @alerts=1, @locked=0, @userid={1}", modId, userId));
-                   Assert.IsTrue(reader.HasRows);
-               }
-               using (IDnaDataReader reader = StoredProcedureReader.Create("", _connectionDetails))
-               {
-                   reader.ExecuteDEBUGONLY(string.Format("exec getmoderationexlinks @modclassid={0}, @referrals=0, @alerts=1, @locked=0, @userid={1}", modId, TestUserAccounts.GetNormalUserAccount.UserID));
-                   Assert.IsFalse(reader.HasRows);
-               }
+                using (IDnaDataReader reader = StoredProcedureReader.Create("", _connectionDetails))
+                {
+                    reader.ExecuteDEBUGONLY(string.Format("INSERT INTO ExLinkMod ( uri, callbackuri,  status, siteid, lockedby, DateQueued, complainttext, notes) " +
+                        "VALUES ( 'http://www.bbc.co.uk', 'http://www.bbc.co.uk', 0, {0}, {1}, getdate(), '', '')", siteId, userId));
+                    reader.ExecuteDEBUGONLY(string.Format("exec getmoderationexlinks @modclassid={0}, @referrals=0, @alerts=1, @locked=0, @userid={1}", modId, userId));
+                    Assert.IsTrue(reader.HasRows);
+                }
 
+                using (IDnaDataReader reader = StoredProcedureReader.Create("", _connectionDetails))
+                {
+                    reader.ExecuteDEBUGONLY(string.Format("exec getmoderationexlinks @modclassid={0}, @referrals=0, @alerts=1, @locked=0, @userid={1}", modId, TestUserAccounts.GetNormalUserAccount.UserID));
+                    Assert.IsFalse(reader.HasRows);
+                }
+            }
+        }
 
-           }
-       }
+        [TestMethod]
+        public void GetModerationExLinks_GetAlreadyLockedItems_ReturnsOnlyLockedItems()
+        {
+            using (new TransactionScope())
+            {
+                var userId = TestUserAccounts.GetModeratorAccount.UserID;
+                var siteId = 1;
+                int modId = 0;
 
-       [TestMethod]
-       public void GetModerationExLinks_GetAlreadyLockedItems_ReturnsOnlyLockedItems()
-       {
-           using (new TransactionScope())
-           {
-               var userId = TestUserAccounts.GetModeratorAccount.UserID;
-               var siteId = 1;
-               int modId = 0;
-
-               using (IDnaDataReader reader = StoredProcedureReader.Create("", _connectionDetails))
-               {
-                   reader.ExecuteDEBUGONLY("select * from sites where siteid=" + siteId);
-                   Assert.IsTrue(reader.HasRows);
-                   Assert.IsTrue(reader.Read());
-
-
-                   modId = reader.GetInt32NullAsZero("ModClassID");
-               }
-               AddExUrl(siteId);
-
-               using (IDnaDataReader reader = StoredProcedureReader.Create("", _connectionDetails))
-               {
-                   reader.ExecuteDEBUGONLY(string.Format("exec getmoderationexlinks @modclassid={0}, @referrals=0, @alerts=1, @locked=0, @userid={1}", modId, userId));
-                   Assert.IsTrue(reader.HasRows);
-               }
-
-               //add another item
-               AddExUrl(siteId);
-
-               using (IDnaDataReader reader = StoredProcedureReader.Create("", _connectionDetails))
-               {
-                   reader.ExecuteDEBUGONLY(string.Format("exec getmoderationexlinks @modclassid={0}, @referrals=0, @alerts=1, @locked=1, @userid={1}", modId, userId));
-                   Assert.IsTrue(reader.HasRows);
-                   int count = 0;
-                   while (reader.Read())
-                   {
-                       count++;
-                       Assert.AreEqual(userId, reader.GetInt32NullAsZero("LockedBy"));
-                   }
-                   Assert.AreEqual(1, count, "Should only have one item");
-               }
+                using (IDnaDataReader reader = StoredProcedureReader.Create("", _connectionDetails))
+                {
+                    reader.ExecuteDEBUGONLY("select * from sites where siteid=" + siteId);
+                    Assert.IsTrue(reader.HasRows);
+                    Assert.IsTrue(reader.Read());
 
 
-           }
-       }
+                    modId = reader.GetInt32NullAsZero("ModClassID");
+                }
+                AddExUrl(siteId);
 
-       [TestMethod]
-       public void GetModerationExLinks_ItemsInDifferentModClasses_ReturnsOnlyLockedItemsForModClass()
-       {
-           using (new TransactionScope())
-           {
-               var userId = TestUserAccounts.GetModeratorAccount.UserID;
-               var siteId = 1;
-               int modId = 3;
-               int altModId = 0;
-               int altSiteId = 0;
+                using (IDnaDataReader reader = StoredProcedureReader.Create("", _connectionDetails))
+                {
+                    reader.ExecuteDEBUGONLY(string.Format("exec getmoderationexlinks @modclassid={0}, @referrals=0, @alerts=1, @locked=0, @userid={1}", modId, userId));
+                    Assert.IsTrue(reader.HasRows);
+                }
 
-               using (IDnaDataReader reader = StoredProcedureReader.Create("", _connectionDetails))
-               {
-                   reader.ExecuteDEBUGONLY("select top 1 * from sites where ModClassID <> " + modId);
-                   Assert.IsTrue(reader.HasRows);
-                   Assert.IsTrue(reader.Read());
+                //add another item
+                AddExUrl(siteId);
 
-                   altSiteId = reader.GetInt32NullAsZero("siteid");
-                   altModId = reader.GetInt32NullAsZero("ModClassID");
-               }
-               AddExUrl(siteId);
-               AddExUrl(altSiteId);
-
-               using (IDnaDataReader reader = StoredProcedureReader.Create("", _connectionDetails))
-               {
-                   reader.ExecuteDEBUGONLY(string.Format("exec getmoderationexlinks @modclassid={0}, @referrals=0, @alerts=1, @locked=0, @userid={1}", modId, userId));
-                   Assert.IsTrue(reader.HasRows);
-                   int count = 0;
-                   while (reader.Read())
-                   {
-                       count++;
-                       Assert.AreEqual(userId, reader.GetInt32NullAsZero("LockedBy"));
-                       Assert.AreEqual(siteId, reader.GetInt32NullAsZero("SiteId"));
-
-                   }
-                   Assert.AreEqual(1, count, "Should only have one item");
-               }
-           }
-       }
+                using (IDnaDataReader reader = StoredProcedureReader.Create("", _connectionDetails))
+                {
+                    reader.ExecuteDEBUGONLY(string.Format("exec getmoderationexlinks @modclassid={0}, @referrals=0, @alerts=1, @locked=1, @userid={1}", modId, userId));
+                    Assert.IsTrue(reader.HasRows);
+                    int count = 0;
+                    while (reader.Read())
+                    {
+                        count++;
+                        Assert.AreEqual(userId, reader.GetInt32NullAsZero("LockedBy"));
+                    }
+                    Assert.AreEqual(1, count, "Should only have one item");
+                }
 
 
-       private void AddExUrl(int siteId)
-       {
-           using (IDnaDataReader reader = StoredProcedureReader.Create("", _connectionDetails))
-           {
-               reader.ExecuteDEBUGONLY(string.Format("INSERT INTO ExLinkMod ( uri, callbackuri,  status, siteid, DateQueued, complainttext, notes) " +
-                   "VALUES ( 'http://www.bbc.co.uk', 'http://www.bbc.co.uk', 0, {0}, getdate(), '', '')", siteId));
-           }
-       }
-      
+            }
+        }
+
+        [TestMethod]
+        public void GetModerationExLinks_ItemsInDifferentModClasses_ReturnsOnlyLockedItemsForModClass()
+        {
+            using (new TransactionScope())
+            {
+                var userId = TestUserAccounts.GetModeratorAccount.UserID;
+                var siteId = 1;
+                int modId = 3;
+                int altModId = 0;
+                int altSiteId = 0;
+
+                using (IDnaDataReader reader = StoredProcedureReader.Create("", _connectionDetails))
+                {
+                    reader.ExecuteDEBUGONLY("select top 1 * from sites where ModClassID <> " + modId);
+                    Assert.IsTrue(reader.HasRows);
+                    Assert.IsTrue(reader.Read());
+
+                    altSiteId = reader.GetInt32NullAsZero("siteid");
+                    altModId = reader.GetInt32NullAsZero("ModClassID");
+                }
+                AddExUrl(siteId);
+                AddExUrl(altSiteId);
+
+                using (IDnaDataReader reader = StoredProcedureReader.Create("", _connectionDetails))
+                {
+                    reader.ExecuteDEBUGONLY(string.Format("exec getmoderationexlinks @modclassid={0}, @referrals=0, @alerts=1, @locked=0, @userid={1}", modId, userId));
+                    Assert.IsTrue(reader.HasRows);
+                    int count = 0;
+                    while (reader.Read())
+                    {
+                        count++;
+                        Assert.AreEqual(userId, reader.GetInt32NullAsZero("LockedBy"));
+                        Assert.AreEqual(siteId, reader.GetInt32NullAsZero("SiteId"));
+
+                    }
+                    Assert.AreEqual(1, count, "Should only have one item");
+                }
+            }
+        }
+
+
+        private void AddExUrl(int siteId)
+        {
+            using (IDnaDataReader reader = StoredProcedureReader.Create("", _connectionDetails))
+            {
+                reader.ExecuteDEBUGONLY(string.Format("INSERT INTO ExLinkMod ( uri, callbackuri,  status, siteid, DateQueued, complainttext, notes) " +
+                    "VALUES ( 'http://www.bbc.co.uk', 'http://www.bbc.co.uk', 0, {0}, getdate(), '', '')", siteId));
+            }
+        }
+
     }
 }
