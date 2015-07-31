d84e036158467f80ae965cd8f72eb78212400a6d
Lennox M <lennox.mcintosh@bbc.co.uk>
2015-07-31 08:52:41 +0100

    Added delete line, to remove a physical file from the filesystem once db is dropped

--- Main/Source/Shared/TestUtils/RestoreDatabase.sql
+++ Main/Source/Shared/TestUtils/RestoreDatabase.sql
@@ -5,17 +5,17 @@ IF @state = 'RESTORING'
 BEGIN 
 	DROP DATABASE [smallGuideSS] 
 	DROP DATABASE [SmallGuide] 
-RESTORE DATABASE [SmallGuide] FROM 
-DISK = '[SQLROOT]Backup\SmallGuide.bak'
-WITH  FILE = 1,  
-MOVE N'SmallGuide' TO N'[SQLROOT]Data\SmallGuide.mdf',  
-MOVE N'SmallGuide_log' TO N'[SQLROOT]Log\SmallGuide_log.LDF',  
-NOUNLOAD,  REPLACE,  STATS = 1
+	EXECUTE xp_cmdshell 'del [SQLROOT]Data\smallGuideSS.mdf'
+	RESTORE DATABASE [SmallGuide] FROM 
+	DISK = '[SQLROOT]Backup\SmallGuide.bak'
+	WITH  FILE = 1,  
+	MOVE N'SmallGuide' TO N'[SQLROOT]Data\SmallGuide.mdf',  
+	MOVE N'SmallGuide_log' TO N'[SQLROOT]Log\SmallGuide_log.LDF',  
+	NOUNLOAD,  REPLACE,  STATS = 1
 
-
-CREATE DATABASE [smallGuideSS] ON
-( NAME = smallGuide, FILENAME = '[SQLROOT]Data\smallGuideSS.mdf' )
-AS SNAPSHOT OF SmallGuide
+	CREATE DATABASE [smallGuideSS] ON
+	( NAME = smallGuide, FILENAME = '[SQLROOT]Data\smallGuideSS.mdf' )
+	AS SNAPSHOT OF SmallGuide
 
 END
 ELSE
